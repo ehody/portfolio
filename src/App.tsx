c4a721a237e78691f3b5724171d4e896cd0124cb
ehody <ehody81@gmail.com>
2021-12-18 23:06:31 -0800

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import "./App.css";
+import SideBar from "./components/Sidebar/SideBar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Overview from "./Pages/Overview";
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <SideBar />
+        <Routes>
+          <Route path="/overview" element={<Overview/>}></Route>
+        </Routes>
+      </Router>
+    );
+  }
 }
 
 export default App;
